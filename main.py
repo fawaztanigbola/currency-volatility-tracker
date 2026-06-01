127b01507173138436d659a328894deaeedbb69e
tundexf <fawaztanigbola2004@gmail.com>
2026-06-01 21:53:08 +0100

    Completed Project

--- main.py
+++ main.py
@@ -16,34 +16,61 @@ class CurrencyDataPoint:
         self.rate = rate
 
     def __str__(self):
-        return '{} {}'.format(self.date, self.rate)
+        return str(self.rate)
     
     def __repr__(self):
-        return '{} {}'.format(self.date, self.rate)
+        return str(self.rate)
 
 class CurrencyAnalyzer:
+
     def __init__(self):
         self.datapoints = []
+    
+    def get_total_days(self):
+        return len(self.datapoints)
 
     def load_data(self, raw_json, target_currency):
         for rate_date, rate in raw_json['rates'].items():
             self.datapoints.append(CurrencyDataPoint(rate_date, rate[target_currency]))
 
     def calculate_mean(self):
+        if not self.datapoints:
+            return 0.0
+        
+        rate_sum = 0.0
         for datapoint in self.datapoints:
-            return datapoint
+            rate_sum += datapoint.rate
+
+        return rate_sum / self.get_total_days()
 
     def calculate_variance(self):
-        pass
+        total_date = self.get_total_days()
+        if total_date == 0:
+            return 0.0
+        variance_sum = 0.0
+        mean = self.calculate_mean()
+        for datapoint in self.datapoints:            
+            variance_sum += (datapoint.rate - mean)**2
+
+        return variance_sum / total_date
+        
 
     def calculate_volatility(self):
-        pass
+        return self.calculate_variance() ** 0.5
 
-eur_analysis = CurrencyAnalyzer()
+# Prompt user 
+target_currency = input('Input Currency Code: ')
 
-eur_analysis.load_data(data, 'ZAR')
+analysis = CurrencyAnalyzer()
 
-print(eur_analysis.datapoints)
+analysis.load_data(data, target_currency)
+
+print('--- Analysis Of {} Currency ---'.format(target_currency))
+print('Total days analyzed: {}'.format(analysis.get_total_days()))
+print('Average rate: {:.4f}'.format(analysis.calculate_mean()))
+print('Variance: {:.6f}'.format(analysis.calculate_variance()))
+print('Final Volatility metric: {:.6f}'.format(analysis.calculate_volatility()))
+
 
 
 
