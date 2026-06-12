8243e98c67345b9f3346ba7663715eff4b2aca28
Fawaz Tanigbola <fawaztanigbola2004@gmail.com>
2026-06-12 14:33:36 +0100

    python codewars

--- codewars/simple_pig_latin.py
+++ codewars/simple_pig_latin.py
@@ -1,4 +1,11 @@
 def pig_it(text):
+    """
+    Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+    Examples
+    pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+    pig_it('Hello world !')     # elloHay orldway !
+    """
     ans = ""
     for word in text.split():
         if len(word) == 1 and not word.isalpha() :
