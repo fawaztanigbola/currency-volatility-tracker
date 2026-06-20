7f00367dcebee932b963c69e73ae6e4c7608a1b9
Fawaz Tanigbola <fawaztanigbola2004@gmail.com>
2026-06-20 21:28:01 +0100

    codewar python

---
+++ codewars/calculating_with_functions.py
@@ -0,0 +1,55 @@
+def zero(ls=None): 
+    if ls == None:
+        return '0' #your code here
+    else:
+        return int(eval("0" + ls))
+def one(ls=None): 
+    if ls == None:
+        return '1' #your code here
+    else:
+        return int(eval("1" + ls))
+def two(ls=None): 
+    if ls == None:
+        return '2' #your code here
+    else:
+        return int(eval("2" + ls))
+def three(ls=None): 
+    if ls == None:
+        return '3' #your code here
+    else:
+        return int(eval("3" + ls))
+def four(ls=None):
+    if ls == None:
+        return '4' #your code here
+    else:
+        return int(eval("4" + ls))
+def five(ls= None): 
+    if ls == None:
+        return '5' #your code here
+    else:
+        return int(eval("5" + ls))
+def six(ls=None): 
+    if ls == None:
+        return '6' #your code here
+    else:
+        return int(eval("6" + ls))
+def seven(ls=None): 
+    if ls == None:
+        return '7' #your code here
+    else:
+        return int(eval("7" + ls))
+def eight(ls=None): 
+    if ls == None:
+        return '8' #your code here
+    else:
+        return int(eval("8" + ls))
+def nine(ls= None): 
+    if ls == None:
+        return '9' #your code here
+    else:
+        return int(eval("9" + ls))
+
+def plus( rs): return "+ {}".format(rs) #your code here
+def minus( rs ): return "- {}".format(rs) #your code here
+def times( rs ): return "* {}".format(rs) #your code here
+def divided_by( rs ): return "// {}".format(rs) #your code here
